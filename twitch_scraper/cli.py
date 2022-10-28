22c6073d1c6ec4154afbb68a0fcd3b8eadf4cf1a
zigai <ziga.ivansek@gmail.com>
2022-10-28 17:25:49 +0200

    cli

--- twitch_scraper/cli.py
+++ twitch_scraper/cli.py
@@ -1,62 +1,21 @@
-import argparse
-from secrets import *
+def main():
+    from interfacy_cli import CLI
+    from stdl.str_u import FG, ST, colored
 
-from twitch_scraper.scraper import TwitchScraper
+    from twitch_scraper.scraper import TwitchScraper
 
-parser = argparse.ArgumentParser(add_help=True)
-parser.description = "Twitch Scraper"
-parser.add_argument("--client_id", type=str, required=True, help="")
-parser.add_argument("--bearer_token", type=str, required=True, help="")
-parser.add_argument("--save_directory", type=str, required=True, help="")
-sub_parser = parser.add_subparsers(dest="subparser")
-# -----------------------------------------------------
-clips_parser = sub_parser.add_parser("clips")
-clips_parser.description = "Scrape Twitch.tv clips"
-clips_parser.add_argument(
-    "--username",
-    required=False,
-    default=None,
-    type=str,
-    help="username of the broadcaster for whom clips are returned",
-)
-clips_parser.add_argument(
-    "--game",
-    required=False,
-    default=None,
-    type=str,
-    help="name of the game for which clips are returned",
-)
-clips_parser.add_argument(
-    "--started_at",
-    required=False,
-    default=None,
-    type=str,
-    help="starting date/time for returned clips",
-)
-clips_parser.add_argument(
-    "--ended_at",
-    required=False,
-    default=None,
-    type=str,
-    help="ending date/time for returned clips",
-)
-clips_parser.add_argument(
-    "--limit",
-    required=False,
-    default=1000,
-    type=int,
-    help="how many clips to return ",
-)
-# -----------------------------------------------------
-profiles_parser = sub_parser.add_parser("profiles")
-profiles_parser.description = "Scrape Twitch.tv profiles"
-profiles_parser.add_argument(
-    "--usernames",
-    type=list,
-    required=True,
-    help="List of username to scrape. Seperated by ','",
-)
-args = parser.parse_args()
-print(args)
-# How to split arguments between class init method and command?
-# Option #1: in add_argument call add a 'dest' parameter with a string like {owner}.{flag_name}
+    description = """
+    ______           _   __         __         ____                                   
+    /_  __/ _    __  (_) / /_ ____  / /        / __/ ____  ____ ___ _  ___  ___   ____
+    / /   | |/|/ / / / / __// __/ / _ \      _\ \  / __/ / __// _ `/ / _ \/ -_) / __/
+    /_/    |__,__/ /_/  \__/ \__/ /_//_/     /___/  \__/ /_/   \_,_/ / .__/\__/ /_/   
+                                                                    /_/               
+    """
+
+    description = colored(description, FG.MAGENTA, style=ST.BOLD)
+
+    CLI(TwitchScraper, description=description).run()
+
+
+if __name__ == "__main__":
+    main()
